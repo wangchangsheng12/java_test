0f1bc1298ef23add1487cb95ceb307af891e47d2
wangchangsheng12 <1296577801@qq.com>
2022-06-09 19:23:39 +0800

    初始化项目。

---
+++ src/com/demo/controller/hello_test.java
@@ -0,0 +1,9 @@
+package com.demo.controller;
+
+public class hello_test {
+
+    public void sout(){
+        System.out.println("Helloworld");
+    }
+
+}
---
+++ src/com/demo/mainn.java
@@ -0,0 +1,14 @@
+package com.demo;
+
+import com.demo.controller.hello_test;
+
+public class mainn {
+    public static void main(String[] args) {
+        // todoo
+//        hello_test.sout();
+        hello_test sss = new hello_test();
+        sss.sout();
+    }
+}
+//commit
+//push
