b7fd72dc88683ebc36027f46151720ff0e1e592e
wangchangsheng12 <1296577801@qq.com>
2022-06-09 20:02:18 +0800

    新增一个计最大值的功能。

--- src/com/demo/controller/hello_test.java
+++ src/com/demo/controller/hello_test.java
@@ -6,4 +6,15 @@ public class hello_test {
         System.out.println("Helloworld");
     }
 
+    public int calcuate_max(int[] arr) {
+        int length = arr.length;
+        int max_value = 0;
+        for (int i = 0; i < length; i++) {
+            if(arr[i]>max_value){
+                max_value = arr[i];
+            }
+        }
+        return max_value;
+    }
+
 }
--- src/com/demo/mainn.java
+++ src/com/demo/mainn.java
@@ -3,12 +3,21 @@ package com.demo;
 import com.demo.controller.hello_test;
 
 public class mainn {
-    public static void main(String[] args) {
-        // todoo
-//        hello_test.sout();
-        hello_test sss = new hello_test();
+
+    public static final hello_test sss= new hello_test();
+    public static final int[] arr = {1,2,3,4,5};
+
+    public  static void test1(){
         sss.sout();
     }
+
+    public static void test2(){
+        int result = sss.calcuate_max(arr);
+        System.out.println(result);
+    }
+
+    public static void main(String[] args) {
+//        test1();
+        test2();
+    }
 }
-//commit
-//push
